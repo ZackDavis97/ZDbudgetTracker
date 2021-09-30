2531af580c9e29fda608d42c3c20b3b7effa600f
Zack Davis <jantrezac95@gmail.com>
2021-09-30 16:40:49 -0500

    more db.js

--- public/db.js
+++ public/db.js
@@ -18,3 +18,37 @@ request.onsuccess = function (event) {
 request.onerror = function (event) {
   console.log("failed " + event.target.errorCode);
 };
+
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => response.json())
+          .then(() => {
+            const transaction = db.transaction(["pending"], "readwrite");
+            const store = transaction.objectStore("pending");
+            store.clear();
+          });
+      }
+    };
+  }
+
+  function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+  }
+  
+
+  window.addEventListener("online", checkDatabase);
